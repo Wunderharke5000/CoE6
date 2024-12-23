55b2e9175b6dc8546cf43e96b0318a080ed4cacd
Arved Steuer <arvedsteuer@gmail.com>
2024-12-23 18:05:39 +0100

    Ensured heldnuildingtest doesnt cause error

---
+++ CoE6/CoE6/ContentLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CoE6;
+
+public static class ContentLoader
+{
+    public static List<Texture2D> textures;
+    static ContentLoader()
+    {
+        textures = new List<Texture2D>();
+    }
+}
---
+++ CoE6/CoE6/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new CoE6.Game1();
+game.Run();
